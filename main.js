09da6d1219c934b062cf54d413e527fb26ddbb3f
akshay85131 <akshay85131@gmail.com>
2020-01-31 12:26:57 +0530

    tested app with just youtube embeded code

---
+++ main.js
@@ -0,0 +1,18 @@
+const electron = require('electron')
+const { app, BrowserWindow } = electron
+let win
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
